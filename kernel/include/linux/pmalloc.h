2aa0d5e52dacea0287b959f87ee2d69e54bb7144
JerryYOJ <95299149+JerryYOJ@users.noreply.github.com>
2023-08-28 13:50:23 +0800

    JUST TURN OFF PMALLOC !!!

--- kernel/include/linux/pmalloc.h
+++ kernel/include/linux/pmalloc.h
@@ -12,7 +12,7 @@
 
 #ifndef _PMALLOC_H
 #define _PMALLOC_H
-#ifdef CONFIG_HISI_PMALLOC
+#if 0
 
 
 #include <linux/genalloc.h>
@@ -20,43 +20,43 @@
 
 #define PMALLOC_DEFAULT_ALLOC_ORDER (-1)
 
-/*
- * Library for dynamic allocation of pools of memory that can be,
- * after initialization, marked as read-only.
- *
- * This is intended to complement __read_only_after_init, for those cases
- * where either it is not possible to know the initialization value before
- * init is completed, or the amount of data is variable and can be
- * determined only at run-time.
- *
- * ***WARNING***
- * The user of the API is expected to synchronize:
- * 1) allocation,
- * 2) writes to the allocated memory,
- * 3) write protection of the pool,
- * 4) freeing of the allocated memory, and
- * 5) destruction of the pool.
- *
- * For a non-threaded scenario, this type of locking is not even required.
- *
- * Even if the library were to provide support for locking, point 2)
- * would still depend on the user taking the lock.
- */
+ /*
+  * Library for dynamic allocation of pools of memory that can be,
+  * after initialization, marked as read-only.
+  *
+  * This is intended to complement __read_only_after_init, for those cases
+  * where either it is not possible to know the initialization value before
+  * init is completed, or the amount of data is variable and can be
+  * determined only at run-time.
+  *
+  * ***WARNING***
+  * The user of the API is expected to synchronize:
+  * 1) allocation,
+  * 2) writes to the allocated memory,
+  * 3) write protection of the pool,
+  * 4) freeing of the allocated memory, and
+  * 5) destruction of the pool.
+  *
+  * For a non-threaded scenario, this type of locking is not even required.
+  *
+  * Even if the library were to provide support for locking, point 2)
+  * would still depend on the user taking the lock.
+  */
 
 
-/**
- * pmalloc_create_pool - create a new protectable memory pool -
- * @name: the name of the pool, must be unique
- * @min_alloc_order: log2 of the minimum allocation size obtainable
- *                   from the pool
- *
- * Creates a new (empty) memory pool for allocation of protectable
- * memory. Memory will be allocated upon request (through pmalloc).
- *
- * Returns a pointer to the new pool upon success, otherwise a NULL.
- */
-struct gen_pool *pmalloc_create_pool(const char *name,
-					 int min_alloc_order);
+  /**
+   * pmalloc_create_pool - create a new protectable memory pool -
+   * @name: the name of the pool, must be unique
+   * @min_alloc_order: log2 of the minimum allocation size obtainable
+   *                   from the pool
+   *
+   * Creates a new (empty) memory pool for allocation of protectable
+   * memory. Memory will be allocated upon request (through pmalloc).
+   *
+   * Returns a pointer to the new pool upon success, otherwise a NULL.
+   */
+struct gen_pool* pmalloc_create_pool(const char* name,
+	int min_alloc_order);
 
 /**
  * pmalloc_prealloc - tries to allocate a memory chunk of the requested size
@@ -72,7 +72,7 @@ struct gen_pool *pmalloc_create_pool(const char *name,
  *
  * Returns true if the vmalloc call was successful, false otherwise.
  */
-bool pmalloc_prealloc(struct gen_pool *pool, size_t size);
+bool pmalloc_prealloc(struct gen_pool* pool, size_t size);
 
 /**
  * pmalloc - allocate protectable memory from a pool
@@ -89,7 +89,7 @@ bool pmalloc_prealloc(struct gen_pool *pool, size_t size);
  * Returns the pointer to the memory requested upon success,
  * NULL otherwise (either no memory available or pool already read-only).
  */
-void *pmalloc(struct gen_pool *pool, size_t size, gfp_t gfp);
+void* pmalloc(struct gen_pool* pool, size_t size, gfp_t gfp);
 
 
 /**
@@ -104,7 +104,7 @@ void *pmalloc(struct gen_pool *pool, size_t size, gfp_t gfp);
  * Returns the pointer to the zeroed memory requested, upon success,
  * NULL otherwise (either no memory available or pool already read-only).
  */
-static inline void *pzalloc(struct gen_pool *pool, size_t size, gfp_t gfp)
+static inline void* pzalloc(struct gen_pool* pool, size_t size, gfp_t gfp)
 {
 	return pmalloc(pool, size, gfp | __GFP_ZERO);
 }
@@ -119,13 +119,13 @@ static inline void *pzalloc(struct gen_pool *pool, size_t size, gfp_t gfp)
  *
  * Returns either NULL or the pmalloc result.
  */
-static inline void *pmalloc_array(struct gen_pool *pool, size_t n,
-				  size_t size, gfp_t flags)
+static inline void* pmalloc_array(struct gen_pool* pool, size_t n,
+	size_t size, gfp_t flags)
 {
 	size_t total_size = n * size;
 
 	if (unlikely(!(pool && n && size && (total_size / n == size))))
-	    return NULL;
+		return NULL;
 	return pmalloc(pool, n * size, flags);
 }
 
@@ -139,8 +139,8 @@ static inline void *pmalloc_array(struct gen_pool *pool, size_t n,
  *
  * Returns either NULL or the pmalloc result.
  */
-static inline void *pcalloc(struct gen_pool *pool, size_t n,
-			    size_t size, gfp_t flags)
+static inline void* pcalloc(struct gen_pool* pool, size_t n,
+	size_t size, gfp_t flags)
 {
 	return pmalloc_array(pool, n, size, flags | __GFP_ZERO);
 }
@@ -156,10 +156,10 @@ static inline void *pcalloc(struct gen_pool *pool, size_t n,
  *
  * Returns a pointer to the replica, NULL in case of recoverable error.
  */
-static inline char *pstrdup(struct gen_pool *pool, const char *s, gfp_t gfp)
+static inline char* pstrdup(struct gen_pool* pool, const char* s, gfp_t gfp)
 {
 	size_t len;
-	char *buf;
+	char* buf;
 
 	if (unlikely(pool == NULL || s == NULL))
 		return NULL;
@@ -180,7 +180,7 @@ static inline char *pstrdup(struct gen_pool *pool, const char *s, gfp_t gfp)
  *
  * Returns 0 upon success, -EINVAL in abnormal cases.
  */
-int pmalloc_protect_pool(struct gen_pool *pool);
+int pmalloc_protect_pool(struct gen_pool* pool);
 
 /**
  * pfree - mark as unused memory that was previously in use
@@ -197,7 +197,7 @@ int pmalloc_protect_pool(struct gen_pool *pool);
  * The freed memory, in this case, will be truly released only when the
  * pool is destroyed.
  */
-void pfree(struct gen_pool *pool, const void *addr);
+void pfree(struct gen_pool* pool, const void* addr);
 
 /**
  * pmalloc_destroy_pool - destroys a pool and all the associated memory
@@ -207,77 +207,77 @@ void pfree(struct gen_pool *pool, const void *addr);
  *
  * Returns 0 upon success, -EINVAL in abnormal cases.
  */
-int pmalloc_destroy_pool(struct gen_pool *pool);
+int pmalloc_destroy_pool(struct gen_pool* pool);
 
 #else
 #include <linux/slab.h>
 #include <linux/err.h>
 
-/**
- * These are an alternate version of the pmalloc API, meant mostly for
- * debugging.
- */
+ /**
+  * These are an alternate version of the pmalloc API, meant mostly for
+  * debugging.
+  */
 
 #define ERR_PMALLOC_OFF 1
 static inline
-struct gen_pool *pmalloc_create_pool(const char *name,
-				     int min_alloc_order)
+struct gen_pool* pmalloc_create_pool(const char* name,
+	int min_alloc_order)
 {
 	return ERR_PTR(ERR_PMALLOC_OFF);
 }
 
 static inline
-bool pmalloc_prealloc(struct gen_pool *pool, size_t size)
+bool pmalloc_prealloc(struct gen_pool* pool, size_t size)
 {
 	return true;
 }
 
 static inline
-void *pmalloc(struct gen_pool *pool, size_t size, gfp_t gfp)
+void* pmalloc(struct gen_pool* pool, size_t size, gfp_t gfp)
 {
 	return kmalloc(size, gfp);
 }
 
 static inline
-void *pzalloc(struct gen_pool *pool, size_t size, gfp_t gfp)
+void* pzalloc(struct gen_pool* pool, size_t size, gfp_t gfp)
 {
 	return kzalloc(size, gfp);
 }
 
 static inline
-void *pmalloc_array(struct gen_pool *pool, size_t n,
-		    size_t size, gfp_t flags)
+void* pmalloc_array(struct gen_pool* pool, size_t n,
+	size_t size, gfp_t flags)
 {
 	return kmalloc_array(n, size, flags);
 }
 
 static inline
-void *pcalloc(struct gen_pool *pool, size_t n,
-		    size_t size, gfp_t flags)
+void* pcalloc(struct gen_pool* pool, size_t n,
+	size_t size, gfp_t flags)
 {
 	return kcalloc(n, size, flags);
 }
 
 static inline
-char *pstrdup(struct gen_pool *pool, const char *s, gfp_t gfp)
+char* pstrdup(struct gen_pool* pool, const char* s, gfp_t gfp)
 {
 	return kstrdup(s, gfp);
 }
 
 static inline
-int pmalloc_protect_pool(struct gen_pool *pool)
+int pmalloc_protect_pool(struct gen_pool* pool)
 {
 	return 0;
 }
 
 static inline
-void pfree(struct gen_pool *pool, const void *addr)
+void pfree(struct gen_pool* pool, const void* addr)
 {
 	kfree(addr);
 }
 
 static inline
-int pmalloc_destroy_pool(struct gen_pool *pool)
+int pmalloc_destroy_pool(struct gen_pool* pool)
 {
 	return 0;
 }
